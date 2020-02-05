a84de7949f395ca25001ad74c347e06898e0c2bf
Chris Canipe <chris.canipe@thomsonreuters.com>
2020-02-05 11:58:31 -0500

    updating v

--- scripts/trElex.js
+++ scripts/trElex.js
@@ -1,14 +1,13 @@
-
 import * as elexUtils from "./elexUtils.js";
 import makeMap from "./makeMap.js";
 import makeTable from "./makeTable.js";
 import setTooltip from "./tooltip.js";
 
 let trElex = {
-	elexUtils:elexUtils,
-	makeMap, makeMap,
-	makeTable, makeTable,
-	setTooltip, setTooltip
+    elexUtils: elexUtils,
+    makeMap: makeMap,
+    makeTable: makeTable,
+    setTooltip: setTooltip
 }
 
 export default trElex;
