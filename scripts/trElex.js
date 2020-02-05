8b56479a3df134f3aba333e6ad6170f447b116e9
Chris Canipe <chris.canipe@thomsonreuters.com>
2020-02-05 12:30:42 -0500

    updating v

--- scripts/trElex.js
+++ scripts/trElex.js
@@ -3,11 +3,14 @@ import makeMap from "./makeMap.js";
 import makeTable from "./makeTable.js";
 import setTooltip from "./tooltip.js";
 
+import taskbar from '../templates/taskbar.html';
+
 let trElex = {
     elexUtils: elexUtils,
     makeMap: makeMap,
     makeTable: makeTable,
-    setTooltip: setTooltip
+    setTooltip: setTooltip,
+    taskbar: taskbar
 }
 
 export default trElex;
